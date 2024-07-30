8557ec39ef19aca5b13f6309be7f67b0a4508966
alderjunio <alder.junio@gmail.com>
2024-07-30 09:20:02 -0400

    git push projeto

---
+++ src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end; 
+    
+    font-size: 24px;
+    font-family: 'Arial';
+      
+   
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        display: flex;
+        align-items: flex-end;
+        flex-direction: row;
+        padding: 0 10px;
+
+        font-size: 24px;
+        color: #ffffff;
+    }
+`
